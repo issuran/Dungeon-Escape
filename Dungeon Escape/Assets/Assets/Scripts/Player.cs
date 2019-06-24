f1bcaa1cdced7c777f05dae3188e9c2c0a9a20e7
Tiago Oliveira <oliveiratif@gmail.com>
2019-06-23 22:36:44 -0300

    Getting player movement

---
+++ Dungeon Escape/Assets/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody2D _rigid;
+	// Use this for initialization
+	void Start () {
+        _rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        _rigid.velocity = new Vector2(horizontalInput, _rigid.velocity.y);
+	}
+}
