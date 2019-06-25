8666bb86ad2fc7878f9525d64fc3e7ef24200298
Tiago Oliveira <oliveiratif@gmail.com>
2019-06-24 22:26:00 -0300

    Handling jump and raycast

--- Dungeon Escape/Assets/Assets/Scripts/Player.cs
+++ Dungeon Escape/Assets/Assets/Scripts/Player.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     private Rigidbody2D _rigid;
+    [SerializeField]
+    private float _jumpForce = 5.0f;
+    [SerializeField]
+    private bool _grounded = false;
+    [SerializeField]
+    private LayerMask _groundLayer;
 	// Use this for initialization
 	void Start () {
         _rigid = GetComponent<Rigidbody2D>();
@@ -12,7 +18,21 @@ public class Player : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        _rigid.velocity = new Vector2(horizontalInput, _rigid.velocity.y);
+        float move = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && _grounded == true)
+        {
+            _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
+            _grounded = false;
+        }
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, _groundLayer.value);
+        Debug.DrawRay(transform.position, Vector2.down * 0.6f, Color.green);
+        if (hitInfo.collider != null)
+        {
+            Debug.Log("Hit: " + hitInfo.collider.name);
+            _grounded = true;
+        }
+        _rigid.velocity = new Vector2(move, _rigid.velocity.y);
 	}
 }
